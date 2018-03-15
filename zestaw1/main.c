d8f57916b528d66383d7cf9de72679a7e472380d
ajopek <arjopek@gmail.com>
2018-03-15 23:15:17 +0100

    Add program arguments handling in main

--- zestaw1/main.c
+++ zestaw1/main.c
@@ -12,19 +12,60 @@ string_array* create(int size, int block_size);
 string_array* create_with_content(int size, int block_size);
 void add_and_delete(string_array* array_struct, int number_of_blocks);
 void search_element(string_array* array_struct, int value);
+void add(string_array* array_struct, int number_of_blocks);
+void delete(string_array* array_struct, int number_of_blocks);
 // Time measurement --------------------------
 // Helper functions --------------------------
 void random_string(int length, char* string);
 void fill_random_content(string_array* array);
+void print_help();
 // -------------------------------------------
 
 int main(int argc, char* argv[]) {
-
-
-
     srand((unsigned int)time(0));
-    string_array* array = create_with_content(12, 10);
-    search_element(array, 5);
+    string_array* array_with_content;
+    string_array* array;
+    /** ------------------------------------------------------
+     *  Program arguments handling
+     * -------------------------------------------------------
+     */
+    int i;
+    int argv_i = 1;
+    int created = 0;
+    int num_arg;
+    if(argc <= 3){
+        print_help();
+        return 0;
+    }
+    for (i = 0; i < argc && i < 3; ++i) {
+        if(strcmp("create_array", argv[argv_i]) == 0 && !created){
+            int size = atoi(argv[++argv_i]);
+            int block_size = atoi(argv[++argv_i]);
+            array_with_content = create_with_content(size, block_size);
+            array = create(size, block_size);
+            if(array_with_content == NULL || array == NULL) return 0;
+            created = 1;
+        } else if(strcmp("add", argv[argv_i]) == 0 && created) {
+            num_arg = atoi(argv[++argv_i]);
+            add(array, num_arg);
+        } else if(strcmp("delete", argv[argv_i]) == 0 && created) {
+            num_arg = atoi(argv[++argv_i]);
+            delete(array_with_content, num_arg);
+        } else if(strcmp("search", argv[argv_i]) == 0 && created) {
+            num_arg = atoi(argv[++argv_i]);
+            search_element(array_with_content, num_arg);
+        } else if(strcmp("add_and_delete", argv[argv_i]) == 0 && created) {
+            num_arg = atoi(argv[++argv_i]);
+            add_and_delete(array, num_arg);
+        } else {
+            print_help();
+            break;
+        }
+        argv_i ++;
+    }
+
+    if(array != NULL) delete_array(array);
+    if(array_with_content != NULL) delete_array(array_with_content);
     return 0;
 }
 
@@ -49,6 +90,7 @@ string_array* create_table(int size, int block_size) {
     string_array* result = (string_array *) malloc(sizeof(string_array));
     if(init(DYNAMIC, (size_t) size, &result)){
         printf("Allocating dynamic array failed");
+        return NULL;
     }
     return result;
  }
@@ -80,6 +122,26 @@ void add_and_delete(string_array* array_struct, int number_of_blocks) {
     free(string);
 };
 
+void add(string_array* array_struct, int number_of_blocks){
+    int i;
+    printf("Adding random %i blocks \n", number_of_blocks);
+    char *string = malloc((array_struct -> block_size) * sizeof(char));
+    for (i = 0; i < number_of_blocks ; ++i) {
+        int index = rand() % (int) array_struct->size;
+        random_string(array_struct -> block_size, string);
+        add_block(array_struct, index, string);
+    }
+    free(string);
+}
+
+void delete(string_array* array_struct, int number_of_blocks){
+    int i;
+    printf("Delete random %i blocks \n", number_of_blocks);
+    for (i = 0; i < number_of_blocks ; ++i) {
+        int index = rand() % (int) array_struct->size;
+        delete_block(array_struct, index);
+    }
+}
 
 /** ---------------------------------------------------------------------------------------
  * Helper functions
@@ -105,3 +167,14 @@ void fill_random_content(string_array* array) {
     }
     free(string);
 };
+
+void print_help(){
+    printf("Usage of program: \n"
+                   "Program will take 3 arguments, which specify operations to test. \n"
+                   "Avaliable operations: \n"
+                   "create_table size block_size - needs to be the first operation \n"
+                   "add number - add number of random blocks \n"
+                   "delete number - delete number of random blocks \n"
+                   "search value - search for block with nearest ascii sum to value \n"
+                   "add_and_delete number - add and delete number of random blocks");
+}
