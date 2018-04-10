75df7276ddba291abb0ead6e2b4886946ff0d7eb
ajopek <arjopek@gmail.com>
2018-04-10 08:37:21 +0200

    Fix main2 in zad2 zestaw4

--- zestaw4/zad2/main2.c
+++ zestaw4/zad2/main2.c
@@ -10,10 +10,11 @@
 
 
 pid_t *PIDS;
-int PROCESS_N;
-int CNT = 0;
+pid_t *CHILDREN;
+sig_atomic_t CNT = 0;
 int KBUFFER_SIZE=0;
-int cntrl = 0;
+sig_atomic_t exit_flag = 0;
+sig_atomic_t ALIVE = 0;
 
 void print_program_error(int error_code);
 int parse(int argc, char *argv[], int *N, int *K);
@@ -23,6 +24,8 @@ void handler_sigusr(int sig_num, siginfo_t *sig_info, void *sig_context);
 void handler_sigrt(int sig_num, siginfo_t *sig_info, void *sig_context);
 void handler_sigint();
 void handler_sigusr2();
+void handler_sigint();
+void kill_remaining();
 
 int main(int argc, char *argv[])
 {
@@ -32,16 +35,25 @@ int main(int argc, char *argv[])
         return result;
     }
 
-    result = create_processes(N, K);
+    create_processes(N, K);
+    free(PIDS);
+    free(CHILDREN);
 
     return 0;
 }
 
 int parse(int argc, char *argv[], int *N, int *K){
-    if(argc < 3 ) return 1;
+    if(argc < 4 ) return 1;
     if((*N = atoi(argv[1])) == 0) return 2;
     if ((*K = atoi(argv[2])) == 0)  return 3;
+    
     PIDS = malloc(*K * sizeof(pid_t));
+    CHILDREN = malloc((*N + 1) * sizeof(pid_t) );
+    CHILDREN[*N] = (pid_t)0;
+    int i;
+    for(i = 0; i < *N; ++i) {
+        CHILDREN[i] = -1;
+    }
     KBUFFER_SIZE = *K;
     return 0;
 
@@ -55,13 +67,13 @@ void print_program_error(int error_code)
             printf("succesfully done all operations\n");
             break;
         case 1:
-            printf("./program NumberOfProcess(N) NumberOfWaitedProcess(K) \n");
+            printf("./zad2 NumberOfProcesses(N) NumberOfWaitedProcesses(K) \n");
             break;
         case 2:
-            printf("provided N is not a number \n");
+            printf("provided N is not valid \n");
             break;
         case 3:
-            printf("provided K is not a number \n");
+            printf("provided K is not valid \n");
             break;
     }
 }
@@ -69,30 +81,36 @@ void print_program_error(int error_code)
 int create_processes(int N, int K){
     set_handlers();
     for(int i=0; i < N ; i++){
-        srand(time(NULL));
         int curr_pid;
         curr_pid = fork();
+        srand(time(NULL) + curr_pid);
         if (curr_pid == 0){
-            int wait_time;
+            int rndm;
             struct sigaction act;
             memset(&act, 0, sizeof(act));
             sigemptyset(&act.sa_mask);
             act.sa_flags = SA_SIGINFO | SA_RESTART;
             act.sa_sigaction = handler_sigusr2;
             sigaction(SIGUSR2, &act, NULL);
-            sleep(wait_time = rand()%10 + 1);
+            sleep(rndm = rand()%10 + 1);
             kill(getppid(), SIGUSR1);
             pause();
             union sigval a;
-            a.sival_int = wait_time;
+            a.sival_int = rndm;
             sigqueue(getppid(), rand() % (SIGRTMAX - SIGRTMIN) + SIGRTMIN, a);
-            exit(wait_time);
+            exit(rndm);
         }else{
             sleep(1);
+            ALIVE++;
         }
     }
-    while(CNT){}
-    printf("%d\n", CNT);
+
+    while(ALIVE > 0){
+        if(exit_flag) {
+            kill_remaining();
+            break;
+        }
+    }
     return 0;
 }
 
@@ -100,7 +118,8 @@ void set_handlers(){
     struct sigaction act;
     memset(&act, 0, sizeof(act));
     sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
+    sigfillset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO | SA_RESTART;
     act.sa_sigaction = handler_sigusr;
     sigaction(SIGUSR1, &act, NULL);
 
@@ -108,6 +127,9 @@ void set_handlers(){
     for(int i=SIGRTMIN; i<=SIGRTMAX; i++){
         sigaction(i, &act, NULL);
     }
+
+    act.sa_sigaction = &handler_sigint;
+    sigaction(SIGINT, &act, NULL);
 }
 
 
@@ -130,10 +152,30 @@ void handler_sigusr2(){ // ignored
 }
 
 void handler_sigrt(int sig_num, siginfo_t *sig_info, void *sig_context){
-    int exit_status;
-    waitpid(sig_info->si_pid, &exit_status, 1);
-    exit_status = WEXITSTATUS(exit_status);
-    printf("<%d> ended child <%d> ended with %d\n", getpid(), sig_info->si_pid,\
-            exit_status);
-    CNT--;
+    printf("Got signal SIGMIN+%i from %i \n", sig_num - SIGRTMIN, sig_info->si_pid);
+    int status, i;
+    waitpid(sig_info->si_pid, &status, 1);
+    status = WEXITSTATUS(status);
+    printf("Child %i, exited with status %i \n", sig_info->si_pid, status);
+    ALIVE--;
+    for(i = 0; CHILDREN[i] != (pid_t)0; ++i) {
+        if(CHILDREN[i] == sig_info->si_pid) {
+            CHILDREN[i] = -1;
+        }
+    }
 }
+
+void handler_sigint() {
+    exit_flag = 1;
+}
+
+void kill_remaining() {
+    int i;
+    for(i = 0; CHILDREN[i] != (pid_t)0; ++i) {
+        if(CHILDREN[i] != -1) {
+            kill(SIGKILL, CHILDREN[i]);
+        }
+    }
+}
+
+
